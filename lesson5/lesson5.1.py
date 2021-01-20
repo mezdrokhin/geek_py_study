16d71801fac947e78c48a3693693de4231a078f9
mezdrokhin <mezdrokhin@mail.ru>
2021-01-21 00:22:57 +0300

    Доделал домашку 6

--- lesson5/lesson5.1.py
+++
@@ -1,6 +0,0 @@
-with open("my_file.txt", 'w') as f_1:
-    data ='.'
-    while data !='':
-        data=input('vvedite stroky: ')
-        f_1.write(data)
-        f_1.write('\n')
--- lesson5/lesson5.5.py
+++
@@ -1,8 +0,0 @@
-str=('324 6 4 5 7 3 35 6 5434 5 6')
-with open("file5.txt", 'w') as f5:
-    f5.write(str)
-with open("file5.txt", 'r') as f5:
-    content = f5.read()
-new=content.split(' ')
-newint=map(int,new)
-print('summa = ', sum(newint))
